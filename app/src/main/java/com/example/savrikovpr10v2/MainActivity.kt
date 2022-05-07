c91b2eeaf04a4a33f90993c836a58a7466b7761e
A GHETTO CHRISTMAS CAROL <ivansavrikow@gmail.com>
2022-05-07 21:44:10 +0700

    два экрана

--- app/src/main/java/com/example/savrikovpr10v2/MainActivity.kt
+++ app/src/main/java/com/example/savrikovpr10v2/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.savrikovpr10v2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnGoOver = findViewById<Button>(R.id.GoOver);
+
+        btnGoOver.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/savrikovpr10v2/screan_2.kt
@@ -0,0 +1,11 @@
+package com.example.savrikovpr10v2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class screan_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screan2)
+    }
+}
